472dbba851a54d4f452f1bf6e519a603bb92dd77
hansolChoi29 <ggksthf29@gmail.com>
2025-04-20 23:45:07 +0900

    feat:login 브랜치 생성하고 바로 commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,10 @@ export default function LoginPage() {
   return (
     <main className="min-h-screen flex font-sans">
       {/* 이 안의 텍스트는 system‑ui, sans‑serif → 글로벌 폰트 덮어쓰기 */}
-      <form className="w-full max-w-sm">dsd</form>
+      <form className="w-full max-w-sm">
+        <h1>로그인</h1>
+        <p>회원정보를 입력해 주세요</p>
+      </form>
     </main>
   );
 }
