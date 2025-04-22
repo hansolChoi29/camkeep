6ff17284c9dbd2f8d599624d38c765735a6d1c0d
hansolChoi29 <ggksthf29@gmail.com>
2025-04-22 10:16:24 +0900

    feat: 장비api에서 제공되는 목록 더 불러와서 뼈대에 넣음

--- src/app/equipment-list/_components/equipmenList.client.tsx
+++ src/app/equipment-list/_components/equipmenList.client.tsx
@@ -11,8 +11,18 @@ export interface NaverItem {
   title: string;
   link: string;
   image: string;
-  lprice: string;
-  mallName: string;
+  lprice: number; // 최저가
+  hprice?: number; // 최고가 (선택)
+  mallName: string; // 쇼핑몰 이름
+  productId: string; // 상품 고유 ID
+  productType: string; // 상품 타입 ("1"=일반, "2"=렌탈)
+
+  brand?: string; // 브랜드
+  maker?: string; // 제조사
+  category1?: string; // 대분류
+  category2?: string; // 중분류
+  category3?: string; // 소분류
+  category4?: string; // 세분류
 }
 
 export default function EquipmentListClient() {
