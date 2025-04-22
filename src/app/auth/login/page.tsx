7b18d1b13f826a6ec9086dcc2fd86301cfff1304
hansolChoi29 <ggksthf29@gmail.com>
2025-04-22 16:25:26 +0900

    feat: login 로직과 ui 생성

--- src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthClient from "./_components/auth.client";
-
-export default async function LoginPage() {
-  return (
-    <>
-      <AuthClient />
-    </>
-  );
-}
--- src/app/auth/register/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function RegisterPage() {
-  return <>회원가입</>;
-}
