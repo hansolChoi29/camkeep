9dfa1db8e7f2dad84d902f8645b4c7f75b77e4ab
hansolChoi29 <ggksthf29@gmail.com>
2025-04-24 18:32:39 +0900

    style: 커뮤니티 뼈대 생성

---
+++ src/app/community/_components/community.client.tsx
@@ -0,0 +1,8 @@
+export default function CommunityClient() {
+  return (
+    <div>
+      <p>dd</p>
+      <p>dd04241826</p>
+    </div>
+  );
+}
---
+++ src/app/community/page.tsx
@@ -0,0 +1,5 @@
+import CommunityClient from "./_components/community.client";
+
+export default async function CommunityPage() {
+  return <CommunityClient />;
+}
