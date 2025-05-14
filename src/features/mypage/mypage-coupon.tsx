b27613345f4ea6ba6681473cab8cf736e4d9f02f
hansolChoi29 <ggksthf29@gmail.com>
2025-05-14 16:08:26 +0900

    feat: 내가 쓴 게시글 확인하는 UI추가, 클릭 시 동적으로 뺄지 고민하러 감

--- src/features/mypage/mypage-coupon.tsx
+++ src/features/mypage/mypage-coupon.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+
 export default function MypageCoupon() {
-    return (
-        <>MypageCoupon</>
-    )
+  return (
+    <Card>
+      <div>
+        <p></p>
+      </div>
+    </Card>
+  );
 }
