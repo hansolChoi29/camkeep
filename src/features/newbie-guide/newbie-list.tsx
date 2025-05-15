5d422d50562985d7b8a2615527931c4dc264bb42
hansolChoi29 <ggksthf29@gmail.com>
2025-05-15 15:27:38 +0900

    style: accodion 여닫을때 색 변경 삼항연산자

--- src/features/newbie-guide/newbie-list.tsx
+++ src/features/newbie-guide/newbie-list.tsx
@@ -18,19 +18,31 @@ export default function NewbieList() {
       collapsible
       value={openValue}
       onValueChange={setOpenValue}
-      className="space-y-4"
+      className="space-y-1"
     >
       {CATEGORIES.map(({ label, text }) => (
-        <AccordionItem key={label} value={label}>
-          <AccordionTrigger className="flex justify-between items-center text-lg font-semibold text-[#578E7E]">
-            <span>{label}</span>
-          </AccordionTrigger>
-          <AccordionContent>
-            <p className="whitespace-pre-wrap sm:text-base leading-relaxed">
-              {text}
-            </p>
-          </AccordionContent>
-        </AccordionItem>
+        <div
+          key={label}
+          className={`border rounded ${
+            openValue === label ? "border-[#578E7E]" : "border-white"
+          }`}
+        >
+          <AccordionItem key={label} value={label}>
+            <AccordionTrigger
+              className={`px-2 flex justify-between items-center text-lg font-semibold rounded-t pl-2
+                             bg-white text-[#578E7E] 
+                             data-[state=open]:bg-[#578E7E] 
+                             data-[state=open]:text-white `}
+            >
+              <span>{label}</span>
+            </AccordionTrigger>
+            <AccordionContent>
+              <p className="p-3 whitespace-pre-wrap sm:text-base leading-relaxed ">
+                {text}
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        </div>
       ))}
     </Accordion>
   );
