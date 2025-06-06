98e52d9d340dc5e26ab20775cb7abbaa0759bf24
hansolChoi29 <ggksthf29@gmail.com>
2025-06-06 11:25:42 +0900

    style: 동적경로 UI완료

--- src/app/camping/[id]/components/camping.client.tsx
+++ src/app/camping/[id]/components/camping.client.tsx
@@ -12,7 +12,7 @@ export default function CampingDetailClient({
 }: CampingDetailClientProps) {
   return (
     <main className="max-w-4xl mx-auto my-8 space-y-6">
-      <div className="overflow-hidden gap-6 flex flex-col">
+      <div className="overflow-hidden flex flex-col">
         <div className="sm:flex">
           {camp.firstImageUrl && (
             <div className="relative w-full h-64">
@@ -42,87 +42,88 @@ export default function CampingDetailClient({
             </div>
             <div className="sm:text-base text-sm">
               <p className="mt-4 ">주소: {camp.addr1}</p>
-
               <p className="mt-2 ">전화: {camp.tel ?? "정보 없음"}</p>
-              <p className="mt-2 ">예약 방식: {camp.resveCl ?? "정보 없음"}</p>
+              <p className="mt-2 ">
+                예약 방식: {camp.resveCl ?? "정보 없음"}
+              </p>{" "}
+              <p className="mt-2 ">
+                반려동물 : {camp.animalCmgCl ?? "정보 없음"}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-10">
+          <div className="border rounded-b-2xl px-4 py-2 shadow mt-10">
+            <h1>캠핑장 소개</h1>
+            <div className="flex justify-center">
+              <p className="mt-4">
+                소개{" "}
+                {camp.lineIntro ??
+                  camp.intro ??
+                  "아름다운 자연 속에서 편안한 휴식을 즐길 수 있는 캠핑장입니다. 평온한 숲과 맑은 공기, 그리고 별빛 가득한 밤하늘을 느껴보세요."}
+              </p>
+            </div>
+          </div>
+
+          <div className="border rounded-b-2xl px-4 py-2 shadow ">
+            <h1>이용시설</h1>
+            {camp.sbrsCl ? (
+              <ul className="mt-2 flex list-disc list-inside space-x-4 place-content-evenly">
+                {camp.sbrsCl.split(",").map((facility, idx) => (
+                  <li key={idx}>{facility.trim()}</li>
+                ))}
+              </ul>
+            ) : (
+              <p className="mt-2">정보 없음</p>
+            )}
+            {/* <p className="mt-2 "> {camp.posblFcltyCl ?? "정보 없음"}</p> */}
+
+            {/* <p className="mt-2 ">
+            일반 야영장 수 : {camp.gnrlSiteCo ?? "정보 없음"}
+            </p>
+            <p className="mt-2 ">
+            자동차 야영장 수 : {camp.autoSiteCo ?? "정보 없음"}
+            </p>
+            <p className="mt-2 ">
+            카라반 사이트 수 : {camp.caravSiteCo ?? "정보 없음"}
+            </p>
+            <p className="mt-2 ">
+            글램핑 사이트 수 : {camp.glampSiteCo ?? "정보 없음"}
+            </p> */}
+          </div>
+
+          <div className=" rounded-b-2xl px-4 py-2 shadow ">
+            <h1>취사장 형태</h1>
+            <div className="flex justify-center">
+              <p className="mt-2 ">
+                {camp.brazierCl === "개별"
+                  ? "개별 화로대 제공"
+                  : camp.brazierCl === "공용"
+                  ? "공용 화로대 제공"
+                  : "취사 시설 미제공 (개인 취사 도구 지참)"}
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="border rounded-b-2xl px-4 py-2 shadow">
-          <h1>캠핑장 소개</h1>
-          <div className="flex justify-center">
-            <p className="mt-4">
-              소개{" "}
-              {camp.lineIntro ??
-                camp.intro ??
-                "아름다운 자연 속에서 편안한 휴식을 즐길 수 있는 캠핑장입니다. 평온한 숲과 맑은 공기, 그리고 별빛 가득한 밤하늘을 느껴보세요."}
-            </p>
-          </div>
-        </div>
-
-        <div className="border rounded-b-2xl px-4 py-2 shadow ">
-          <h1>이용시설</h1>
-          {camp.sbrsCl ? (
-            <ul className="mt-2 flex list-disc list-inside space-x-4 place-content-evenly">
-              {camp.sbrsCl.split(",").map((facility, idx) => (
-                <li key={idx}>{facility.trim()}</li>
-              ))}
-            </ul>
-          ) : (
-            <p className="mt-2">정보 없음</p>
-          )}
-          {/* <p className="mt-2 "> {camp.posblFcltyCl ?? "정보 없음"}</p> */}
-          <p className="mt-2 ">예약방법 : {camp.resveCl ?? "정보 없음"}</p>
-
-          {/* <p className="mt-2 ">
-            일반 야영장 수 : {camp.gnrlSiteCo ?? "정보 없음"}
-          </p>
-          <p className="mt-2 ">
-            자동차 야영장 수 : {camp.autoSiteCo ?? "정보 없음"}
-          </p>
-          <p className="mt-2 ">
-            카라반 사이트 수 : {camp.caravSiteCo ?? "정보 없음"}
-          </p>
-          <p className="mt-2 ">
-            글램핑 사이트 수 : {camp.glampSiteCo ?? "정보 없음"}
-          </p> */}
-          <p className="mt-2 ">반려동물 : {camp.animalCmgCl ?? "정보 없음"}</p>
-        </div>
-
-        <div className=" rounded-b-2xl px-4 py-2 shadow ">
-          <h1>취사장 형태</h1>
-          <div className="flex justify-center">
-            <p className="mt-2 ">
-              {camp.brazierCl === "개별"
-                ? "개별 화로대 제공"
-                : camp.brazierCl === "공용"
-                ? "공용 화로대 제공"
-                : "취사 시설 미제공 (개인 취사 도구 지참)"}
-            </p>
-          </div>
-        </div>
-
-        <div className="flex flex-col w-full justify-end gap-1 mt-1">
+        <div className="flex w-full justify-center gap-2 mt-20 mb-10">
           <Link
             href={camp.homepage!}
-            className="hover:underline text-[#578E7E]"
+            className=" bg-[#85D962] rounded-lg px-2 py-2 text-white hover:bg-[#54ac2e]"
           >
             홈페이지로 이동
           </Link>
           <Link
             href={camp.resveUrl!}
-            className="hover:underline text-[#578E7E]"
+            className=" bg-[#85D962] rounded-lg px-2 py-2 text-white hover:bg-[#54ac2e]"
           >
             예약사이트로 이동
           </Link>
-        </div>
-        <div className="mb-8 py-2 flex justify-end">
           <Link
             href="/camping"
-            className="text-sm text-[#578E7E] hover:underline"
+            className=" bg-[#578E7E] rounded-lg px-2 py-2 text-white hover:bg-[#178e6c]"
           >
-            ← 캠핑장 목록으로 돌아가기
+            캠핑장 목록으로 돌아가기
           </Link>
         </div>
       </div>
